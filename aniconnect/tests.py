dd7521fe59df8e50c689c470314df3253f7d57b4
ankitgawade <ankitarungawade@gmail.com>
2026-02-19 21:08:42 +0530

    added 2 tests

--- aniconnect/tests.py
+++ aniconnect/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.db import connections
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class DatabaseConnectionTest(SimpleTestCase):
+    """Sanity check that the configured default DB accepts connections."""
+
+    databases = {"default"}
+
+    def test_default_database_connection(self):
+        connection = connections["default"]
+        connection.ensure_connection()
+
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 1")
+            result = cursor.fetchone()
+
+        self.assertEqual(result[0], 1)
+
+
+class SmokeTest(TestCase):
+    def test_home_page_loads(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
